db71431d7c7c83e446d6b8849e47ab88989cb029
Phuy29 <phuchuy292003@gmail.com>
2022-06-13 18:57:57 +0700

    Set up project

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    fontFamily: {
+      oswald: ["Oswald", "sans-serif"],
+    },
+    extend: {
+      fontSize: {
+        13: "13px",
+        14: "14px",
+        16: "16px",
+        20: "20px",
+        26: "26px",
+        24: "24px",
+        32: "32px",
+        36: "36px",
+        48: "48px",
+      },
+    },
+  },
+  plugins: [],
+};
